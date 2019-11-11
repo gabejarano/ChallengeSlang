166d560d10a85d4d9a08b2da6ecc4410356b48b1
Gustavo Bejarano <ga.bejarano10@uniandes.edu.co>
2019-11-11 14:06:50 -0500

    pequeños cambios

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,5 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {App} from './components/App';
 
+//main component
 const app = document.getElementById("app");
-ReactDOM.render(<App title="Spelling Excercise"/>, app )
+ReactDOM.render(<App title="Spelling Exercise"/>, app )
